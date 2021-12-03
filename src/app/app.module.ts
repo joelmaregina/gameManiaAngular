2dbf48dfa68a1537c78fac6ba50972534edf569e
JoelmaRegina <joelma_silvaa@hotmail.com>
2021-12-03 19:55:59 -0300

    uploading the inital Angular structure of GameMania

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CarrinhoComponent } from './pages/carrinho/carrinho.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarrinhoComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,

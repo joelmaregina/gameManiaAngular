3ae313b8b6fa730fe0e8c78ac7f7b8ffcea85753
JoelmaRegina <joelma_silvaa@hotmail.com>
2022-01-16 02:59:05 -0300

    Added more error messages to the login page

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -25,16 +25,20 @@ mensagem = "";
 onSubmit() {
   this.loginService.login(this.loginModel).subscribe( (response) => {
     console.log(response)
-    this.mensagem = "Login com sucesso";
+    this.mensagem = "Login realizado com sucesso!";
     this.router.navigateByUrl("/")
 
   }, (error) => {
     if (error.error == "Incorrect password"){
-      this.mensagem == "Senha Incorreta"
+      this.mensagem == "Senha Incorreta";
     } else if (error.error == "Cannot find user") {
-      this.mensagem = "Usuário não encontrado"
+      this.mensagem = "Usuário não encontrado";
     } else if (error.error == "Password is too short" ) {
-      this.mensagem =  "Senha muito curta"
+      this.mensagem =  "Senha muito curta";
+    } else if(error.error == "Email and password are required"){
+        this.mensagem = " Atenção! Os campos de email e senha não podem estar vazios";
+    } else if (error.error == "Email format is invalid"){
+      this.mensagem = "Formato de email está inválido";
     }
   } )
 }
